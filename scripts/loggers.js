3fbdd09403e948ad33f06e8147d750211d6d22da
HerreraCesar <herrera.cesar.arg@gmail.com>
2023-09-15 21:09:10 -0300

    Test logger

--- scripts/loggers.js
+++ scripts/loggers.js
@@ -7,16 +7,16 @@ export const requestLogger = winston.createLogger({
 export const logger404 = winston.createLogger({
   transports: [
     new winston.transports.Console({ level: "warn" }),
-    new winston.transports.File({ filename: "./logs/warn.log", level: "warn" }),
+    // new winston.transports.File({ filename: "./logs/warn.log", level: "warn" }),
   ],
 });
 
 export const errorLogger = winston.createLogger({
   transports: [
     new winston.transports.Console({ level: "error" }),
-    new winston.transports.File({
-      filename: "./logs/error.log",
-      level: "error",
-    }),
+    // new winston.transports.File({
+    //   filename: "./logs/error.log",
+    //   level: "error",
+    // }),
   ],
 });
